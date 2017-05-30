0be16224678e1ccd4112e2370e98b31514538eb9
Jenkins Continous Integration <jenkins@jenkins-slave-02.ci.atl.primedia.com>
2017-05-30 18:37:13 -0400

    Version 0.1.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.rentpath/rp-query-clj "0.1.2-SNAPSHOT"
+(defproject com.rentpath/rp-query-clj "0.1.2"
   :description "RentPath query abstractions in Clojure"
   :url "https://github.com/rentpath/rp-query-clj"
   :license {:name "Eclipse Public License"
